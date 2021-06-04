3d4010895966f6c8dbc938d51a030ecfe50c1afa
ray-reynolds <ray.reynolds@king.com>
2021-06-04 14:32:33 +0200

    prebuild steps

--- jenkins-jobs/src/jobs/BuildJobApplication1.groovy
+++ jenkins-jobs/src/jobs/BuildJobApplication1.groovy
@@ -5,6 +5,19 @@ mavenJob('Jenkins Tutorial Demo - Application 1 (DSL)') {
         numToKeep 5
     }
 
+    parameters {
+        stringParam('releaseVersion', '',
+                'The release version for the artifact. If you leave this empty, ' +
+                        'the current SNAPSHOT version will be used with the ' +
+                        '"-SNAPSHOT" suffix removed (example: if the current version ' +
+                        'is "1.0-SNAPSHOT", the release version will be "1.0").')
+        stringParam('nextSnapshotVersion', '',
+                'The snapshot version to be used after the release. If you leave ' +
+                        'this empty, the minor version of the release will be ' +
+                        'incremented by one (example: if the release is "1.0", the ' +
+                        'next snapshot version will be "1.1-SNAPSHOT").')
+    }
+
     scm {
         git {
             remote {
@@ -29,13 +42,51 @@ mavenJob('Jenkins Tutorial Demo - Application 1 (DSL)') {
                 extensions << sparseCheckout
                 extensions << pathRestrictions
             }
+
+            extensions {
+                localBranch('master')
+            }
+
         }
+    }
+
+    preBuildSteps {
+        systemGroovyCommand '''
+    import hudson.model.StringParameterValue
+    import hudson.model.ParametersAction
+
+    def env = build.getEnvironment(listener)
+    String releaseVersion = env.get('releaseVersion')
+    String nextSnapshotVersion = env.get('nextSnapshotVersion')
+
+    if (!releaseVersion) {
+      String pomPath = build.workspace.toString() + '/library1/pom.xml'
+      def pom = new XmlSlurper().parse(new File(pomPath))
+      releaseVersion = pom.version.toString().replace('-SNAPSHOT', '')
+      println "releaseVersion (calculated) = $releaseVersion"
+      def param = new StringParameterValue('releaseVersion', releaseVersion)
+      build.replaceAction(new ParametersAction(param))
+    }
+
+    if (!nextSnapshotVersion) {
+      def tokens = releaseVersion.split('\\.')
+      nextSnapshotVersion =
+            tokens[0] + '.' + (Integer.parseInt(tokens[1]) + 1) + '-SNAPSHOT'
+      println "nextSnapshotVersion (calculated) = $nextSnapshotVersion"
+      def param1 = new StringParameterValue('releaseVersion', releaseVersion)
+      def param2 = new StringParameterValue('nextSnapshotVersion',
+            nextSnapshotVersion)
+      build.replaceAction(new ParametersAction(param1, param2))
+    }
+    '''.stripIndent()
     }
 
     triggers {
         scm 'H/15 * * * *'
         snapshotDependencies true
     }
+
+
 
     rootPOM 'library1/pom.xml'
     goals 'clean install'
